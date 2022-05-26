17c187f31b26fda8fc736bea68ec659ac66e1b03
Laurie McLaughlin <lnmclaughlin@gmail.com>
2022-05-26 13:10:42 -0400

    more updates

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -20,127 +20,165 @@ const Details = () => {
   // const isFav: boolean = isThisFav(recipe?.id);
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     getInfoById(id).then((res) => setRecipe(res));
   }, [id]);
   return (
     <>
       {recipe ? (
         <div className="Details">
-          <img className="picture" src={recipe?.image} alt={recipe?.title} />
-          <div
-            className="symbols"
-            onClick={() => console.log("div was clicked")}
-          >
-            {isFav ? (
-              <i
-                className="fa-solid fa-heart fav"
-                onClick={() => deleteFav(recipe?.id!)}
-              ></i>
-            ) : (
-              <i
-                className="fa-solid fa-heart"
-                onClick={() => addFav(recipe!)}
-              ></i>
-            )}
-            <a
-              href="https://www.pinterest.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-pinterest"></i>
-            </a>{" "}
-          </div>
-
-          <a href={recipe?.sourceUrl}>
-            <h2>
-              {/* make link to take to original Url */}
-              {recipe?.title}
-            </h2>
-          </a>
-
-          <div className="icons">
-            {showKey && (
-              <div className="key">
-                {/* <h3> Key: </h3> */}
-                <div>
-                  <p>Vegan: </p>
-                  <span
-                    className="iconify V"
-                    data-icon="emojione-monotone:letter-v"
-                  ></span>
-                </div>
-                <div>
-                  <p>Dairy Free: </p>
-                  <span
-                    className="iconify dairy"
-                    data-icon="mdi:cow-off"
-                  ></span>
-                </div>
-                <div>
-                  <p>Gluten Free: </p>
-                  <span
-                    className="iconify GF"
-                    data-icon="mdi:barley-off"
-                  ></span>
-                </div>
-                <div>
-                  <p>Vegetarian: </p>
-                  <span
-                    className="iconify veggie"
-                    data-icon="charm:plant-pot"
-                  ></span>
-                </div>
+          <div className="section-container">
+            <section className="section-1">
+              <img
+                className="picture"
+                src={recipe?.image}
+                alt={recipe?.title}
+              />
+              <div
+                className="symbols"
+                onClick={() => console.log("div was clicked")}
+              >
+                {isFav ? (
+                  <i
+                    className="fa-solid fa-heart fav"
+                    onClick={() => deleteFav(recipe?.id!)}
+                  ></i>
+                ) : (
+                  <i
+                    className="fa-solid fa-heart"
+                    onClick={() => addFav(recipe!)}
+                  ></i>
+                )}
+                <a
+                  href="https://www.pinterest.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa-brands fa-pinterest"></i>
+                </a>{" "}
               </div>
-            )}
-            <p onClick={() => setShowKey((prev) => !prev)}>
-              <span
-                className="iconify question-mark"
-                data-icon="fluent:book-question-mark-20-regular"
-              ></span>
-            </p>
-            {recipe?.vegan && (
-              <span
-                className="iconify V"
-                data-icon="emojione-monotone:letter-v"
-              ></span>
-            )}
 
-            {recipe?.dairyFree && (
-              <span className="iconify dairy" data-icon="mdi:cow-off"></span>
-            )}
-            {recipe?.glutenFree && (
-              <span className="iconify GF" data-icon="mdi:barley-off"></span>
-            )}
-            {recipe?.vegetarian && (
-              <span
-                className="iconify veggie"
-                data-icon="charm:plant-pot"
-              ></span>
-            )}
+              <h2 className="similar-heading">
+                <a href={recipe?.sourceUrl}>{recipe?.title}</a>
+              </h2>
+
+              <div className="icons">
+                {showKey && (
+                  <div className="key">
+                    {/* <h3> Key: </h3> */}
+                    <div>
+                      <p>Vegan: </p>
+                      <span
+                        className="iconify V"
+                        data-icon="emojione-monotone:letter-v"
+                      ></span>
+                    </div>
+                    <div>
+                      <p>Dairy Free: </p>
+                      <span
+                        className="iconify dairy"
+                        data-icon="mdi:cow-off"
+                      ></span>
+                    </div>
+                    <div>
+                      <p>Gluten Free: </p>
+                      <span
+                        className="iconify GF"
+                        data-icon="mdi:barley-off"
+                      ></span>
+                    </div>
+                    <div>
+                      <p>Vegetarian: </p>
+                      <span
+                        className="iconify veggie"
+                        data-icon="charm:plant-pot"
+                      ></span>
+                    </div>
+                  </div>
+                )}
+                {(recipe?.dairyFree ||
+                  recipe?.glutenFree ||
+                  recipe?.vegetarian ||
+                  recipe?.vegan) && (
+                  <p onClick={() => setShowKey((prev) => !prev)}>
+                    <span
+                      className="iconify question-mark"
+                      data-icon="fluent:book-question-mark-20-regular"
+                    ></span>
+                  </p>
+                )}
+                {recipe?.vegan && (
+                  <p>
+                    <span
+                      className="iconify V"
+                      data-icon="emojione-monotone:letter-v"
+                    ></span>
+                  </p>
+                )}
+
+                {recipe?.dairyFree && (
+                  <p>
+                    <span
+                      className="iconify dairy"
+                      data-icon="mdi:cow-off"
+                    ></span>
+                  </p>
+                )}
+                {recipe?.glutenFree && (
+                  <p>
+                    <span
+                      className="iconify GF"
+                      data-icon="mdi:barley-off"
+                    ></span>
+                  </p>
+                )}
+                {recipe?.vegetarian && (
+                  <p>
+                    <span
+                      className="iconify veggie"
+                      data-icon="charm:plant-pot"
+                    ></span>
+                  </p>
+                )}
+              </div>
+
+              <p className="clock-section">
+                <span
+                  className="iconify clock"
+                  data-icon="akar-icons:clock"
+                ></span>{" "}
+                <span>{recipe?.readyInMinutes}</span>
+              </p>
+            </section>
+            <section className="section-2">
+              <div className="ingredients">
+                <h3>Ingredients:</h3>
+                <ul className="ingredients-list">
+                  {recipe?.extendedIngredients.map((ingredient, index) => (
+                    <Ingredient
+                      key={`${ingredient.id}${index}`}
+                      ingredient={ingredient}
+                    />
+                  ))}
+                </ul>
+              </div>
+              <div className="instructions">
+                <h3>Instructions:</h3>
+                <ol className="instructions-list">
+                  {recipe?.analyzedInstructions.map((instruction) =>
+                    instruction.steps.map((step) => (
+                      <li key={Math.floor(Math.random() * 1000)}>
+                        {step.step}
+                      </li>
+                    ))
+                  )}
+                </ol>
+              </div>
+            </section>
           </div>
-          <p className="clock-section">
-            <span className="iconify clock" data-icon="akar-icons:clock"></span>{" "}
-            <span>{recipe?.readyInMinutes}</span>
-          </p>
-          <div className="ingredients">
-            <h3>Ingredients:</h3>
-            <ul className="ingredients-list">
-              {recipe?.extendedIngredients.map((ingredient) => (
-                <Ingredient key={ingredient.id} ingredient={ingredient} />
-              ))}
-            </ul>
+          <div className="similar">
+            {recipe?.id && <SimilarItems id={recipe?.id!} />}
           </div>
-          <div className="instructions">
-            <h3>Instructions:</h3>
-            <ol>
-              {recipe?.analyzedInstructions.map((instruction) =>
-                instruction.steps.map((step) => (
-                  <li key={Math.floor(Math.random() * 1000)}>{step.step}</li>
-                ))
-              )}
-            </ol>
-          </div>
-          {recipe?.id && <SimilarItems id={recipe?.id!} />}
         </div>
       ) : (
         <p>Loading...</p>
